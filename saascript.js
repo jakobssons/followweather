b093c7f558a9cac79d8f392747b332cd60ceba75
jakobssons <janne.aro@hotmail.com>
2024-01-25 16:38:41 +0200

    Päuivitys 7
    Korjattu kello näyttämään oikean ajan haetussa paikassa.
    Lisätty defaultti taustakuvaan toimintaa ( Näyttää tausta kuvan kellonajan perusteella jos mikää case ei osu)

--- saascript.js
+++ saascript.js
@@ -1,15 +1,20 @@
 const apiKey = 'f998b7e16f0647ffbd4f4cc8431563b5';
 
+function createDateWithOffset(epoch, offset) {
+  const d = new Date()
+  const date = new Date ((epoch + offset + d.getTimezoneOffset()*60) * 1000)
+  return date
+} 
+
 async function getWeatherData(locationName) {
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${locationName}&appid=${apiKey}`;
-  
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
 
     //  Poimitaan tarpeelliset säätiedot
-
-    const weatherData = {locationName: data.name, temperature: data.main.temp, timestamp: new Date(data.dt*1000), description: data.weather[0].main};
+    console.log(data)
+    const weatherData = {locationName: data.name, temperature: data.main.temp, timestamp: createDateWithOffset(data.dt, data.timezone), description: data.weather[0].main};
     return weatherData;
   } catch (error) {
     console.error('Virhe haettaessa säätietoja', error);
@@ -33,6 +38,7 @@ function kelvinToCelsius(kelvin){
   return kelvin - 272.15;
 }
 
+   // Taustakuvat
 async function setWeather(city) {
   const weatherData = await getWeatherData(city);
   let imageUrl = '';
@@ -53,18 +59,19 @@ async function setWeather(city) {
       imageUrl = 'sataavetta.gif';
       break;
 
-    // on yö
-    case 'Night':
-      imageUrl = 'yo.gif';
-      break;
-
     // tuulinen
     case 'Wind':
         imageUrl = 'tuulee.gif';
         break;
 
-      default:
-      imageUrl = 'default.gif'; // Oletus taustakuva
+    //  Defaultit taustat jos mikään aiempi case ei osu
+    //  Valitaan tausta kellonajan mukaan  
+    default:
+      if (weatherData.timestamp.getHours() > 8 && weatherData.timestamp.getHours() < 20){
+        imageUrl = 'default.gif';
+      } else {
+        imageUrl = 'yo.gif';
+      }
   }
 
   // Renderöidään uudet säätiedot
@@ -83,6 +90,7 @@ async function setWeather(city) {
 
 }
 
+//  Enterin toimivuus
 function handleSubmit(e){
   e.preventDefault()
   searchWeather()
